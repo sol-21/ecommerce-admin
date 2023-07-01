bf1b193b4d776530be5093506697c26b7b575308
sol-21 <solomon17bdr@gmail.com>
2023-07-02 03:25:51 +0900

    ecommerce admin with shadcn

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useStoreModal } from '@/hooks/use-store-modal'
+import { Modal } from '@/components/ui/modal'
+
+export const StoreModal = () => {
+	const storeModal = useStoreModal()
+	return (
+		<Modal
+			title='Create store'
+			description='Add a new store to manage products and categories'
+			isOpen={storeModal.isOpen}
+			onClose={storeModal.onClose}>
+			future create store form
+		</Modal>
+	)
+}
